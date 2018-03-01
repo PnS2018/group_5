7dc40313b56ed6056f3676e58f41b8abde866856
mojulian <mojulian@student.ethz.ch>
2018-03-01 17:04:31 +0100

    ex_2 is done

--- session_01/session_1.py
+++ session_01/session_1.py
@@ -4,7 +4,7 @@
 import numpy as np
 from keras import backend as K
 
-# Ex 1 
+# Ex 1
 # your code here
 
 a = K.placeholder(shape=(5,))
@@ -18,12 +18,27 @@ function = K.function(inputs=(a,b,c), outputs(inputs_computed,))
 # Ex 2
 # your code here
 
-x = K.placeholder(shape=())
-inputs_tanh = K.exp(x)+K.exp(-x)/(K.exp(x)-K.exp(-x))) 
+x = K.placeholder(shape=(), dtype)
+inputs_tanh = ((K.exp(x) - K.exp(-x)))/(K.exp(x) +K.exp(-x))
+grad_tanh = K.gradients(loss = tanh, variables = [x])
+
+function_tanh = K.function(inputs = (x,),outputs = (tanh,))
+function_grad_tanh = K.function(inputs =(x,) outputs = (grad_tanh,))
+
+function_tanh((-100,))
+function_tanh((-1,))
+function_tanh((0,))
+function_tanh((1,))
+function_tanh((100,))
+
+function_grad_tanh((-100,))
+function_grad_tanh((-1,))
+function_grad_tanh((0,))
+function_grad_tanh((1,))
+function_grad_tanh((100,))
 
 # Ex 3
 # your code here
 
 # Ex 4
 # your code here
-
